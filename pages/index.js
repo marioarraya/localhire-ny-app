8ce61ef2d19e37a805440170f8dfc64c148afbd4
unknown <marioarraya@gmail.com>
2023-02-07 16:53:40 -0500

    branding and copy changes

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Local Hire New York - Job Board</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Local Hire New York" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We get you hired, trained, and paid.
         </p>
       </main>
 
